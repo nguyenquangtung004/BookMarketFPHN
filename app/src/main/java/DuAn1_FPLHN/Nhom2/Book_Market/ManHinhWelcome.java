aa7960fc59f6127e215a13adb04b97a813582327
Admin <nguyenquangtung04.cv@gmail.com>
2024-03-07 09:30:52 +0700

    Ngày 6/3/2024 đã thêm hộp thoại alert dialog và quên mật khẩu

--- app/src/main/java/DuAn1_FPLHN/Nhom2/Book_Market/ManHinhWelcome.java
+++ app/src/main/java/DuAn1_FPLHN/Nhom2/Book_Market/ManHinhWelcome.java
@@ -10,22 +10,41 @@ import android.widget.TextView;
 import com.airbnb.lottie.LottieAnimationView;
 
 public class ManHinhWelcome extends AppCompatActivity {
-    TextView tvWelcome;
+    TextView tvWelcome, tvSlide;
     LottieAnimationView lottie;
+    private String[] quotes = {"Đọc sách hôm nay, thành công mai sau.",
+            "Cả thế giới mở ra với tôi khi tôi học đọc.",
+            "Đọc sách rất quan trọng. Nếu biết cách đọc, cả thế giới sẽ mở ra với bạn."};
+    private int currentIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_hinh_welcome);
         lottie = findViewById(R.id.lottie_welcome);
         tvWelcome = findViewById(R.id.tv_wc_cm);
-
+        tvSlide = findViewById(R.id.tv_wc_sl);
         animationImage();
-
         animationText();
+        animationText2();
 
 
+    }
 
+    private void animationText2() {
+        final Handler handler = new Handler();
+        final int delay = 1400;
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // Hiển thị đoạn trích hiện tại
+                tvSlide.setText(quotes[currentIndex]);
 
+                // Tăng chỉ số hoặc quay lại nếu đã đến cuối mảng
+                currentIndex = (currentIndex + 1) % quotes.length;
+
+                // Lập lại animation sau khoảng thời gian delay
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
     }
 
     private void animationImage() {
@@ -36,7 +55,7 @@ public class ManHinhWelcome extends AppCompatActivity {
                 Intent intent = new Intent(ManHinhWelcome.this, DangNhap.class);
                 startActivity(intent);
             }
-        },9000);
+        }, 7000);
     }
 
     private void animationText() {
