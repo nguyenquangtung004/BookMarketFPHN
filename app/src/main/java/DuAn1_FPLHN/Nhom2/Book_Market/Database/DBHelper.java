e05332d01937ed387613d2c15c7e5fd9fcd82dc4
Vương Hiến <162457251+Vuonghien02@users.noreply.github.com>
2024-03-20 18:51:28 +0700

    Ngày 20/3 đẩy lên database. Hiến

---
+++ app/src/main/java/DuAn1_FPLHN/Nhom2/Book_Market/Database/DBHelper.java
@@ -0,0 +1,49 @@
+package DuAn1_FPLHN.Nhom2.Book_Market.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context){super(context, "BookMarket", null, 1);}
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //bang san pham
+        String dbSANPHAM = "create table SANPHAM(masp integer primary key autoincrement, tensp text, maloai integer, anhsp blob, motasp text, giasp integer)";
+        db.execSQL(dbSANPHAM);
+        //bang the loai
+        String dbTHELOAI = "CREATE TABLE THELOAI(maloai integer primary key autoincrement, tenloai text)";
+        db.execSQL(dbTHELOAI);
+        //bang gio hang
+        String dbGIOHANG = "create table GIOHANG(id integer primary key autoincrement, tensp text, anhsp blob, giasp integer, soluong integer)";
+        db.execSQL(dbGIOHANG);
+        //bang hoa don
+        String dbHOADON = "create table HOADON(mahd integer primary key autoincrement, ngaylap text, matk integer references TAIKHOAN(matk), hoten text, sdt text, diachi text, tongtien integer, tongsanpham text, trangthai integer)";
+        db.execSQL(dbHOADON);
+        //bang tai khoan
+        String dbTAIKHOAN = "create table TAIKHOAN(matk integer primary key autoincrement, taikhoan text, matkhau text, hoten text, sdt text, diachi text, loaitaikhoan text)";
+        db.execSQL(dbTAIKHOAN);
+
+        //du lieu mau
+
+        db.execSQL("insert into THELOAI values (1, 'Cơm'), (2, 'Cháo'), (3, 'Bún, Phở'), (4, 'Đồ uống')");
+
+        db.execSQL("insert into HOADON values (1, '09/11/2023', 1, 'Lăng Văn Thoại', '0862556262', 'Kim Chung - Hoài Đức - Hà Nội', 90000, 'Cháo gà x1', 0), (2, '03/11/2023', 2, 'Trần Tuấn Đạt', '0979991999', 'Duy Tiên - Hà Nam', 110000, 'Cơm bò x1', 1)");
+
+        db.execSQL("insert into TAIKHOAN values (1, 'admin', '123', 'Vuong Dinh Hien', '0866815906', 'Kim Chung - Hoài Đức - Hà Nội','admin'), (2, 'ad01', '123', 'Trần Tuấn Đạt', '0979991999', 'Duy Tiên - Hà Nam','khách hàng')");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        if (i != i1) {
+            db.execSQL("drop table if exists SANPHAM");
+            db.execSQL("drop table if exists THELOAI");
+            db.execSQL("drop table if exists GIOHANG");
+            db.execSQL("drop table if exists HOADON");
+            db.execSQL("drop table if exists TAIKHOAN");
+            onCreate(db);
+        }
+
+    }
+}
